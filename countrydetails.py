e534c7aa964c00ebcb22bf05947847434362e693
Praveen Goswami <praveen828589@gmail.com>
2022-07-14 03:03:19 +0530

    Added More APIs

--- countrydetails.py
+++ countrydetails.py
@@ -1,11 +1,20 @@
 from countryinfo import CountryInfo
 countryName = input("Please enter country name to fetch its details\n")
 country = CountryInfo(countryName)
-indiaProvinces = country.provinces() #It returns provinces/States list
-indiaAlternateNames = country.alt_spellings() #It returns alternate names
-print("List of all Indian states : \n")
+indiaProvinces = country.provinces()          #This API returns provinces/States list
+indiaAlternateNames = country.alt_spellings() #This API returns alternate names
+indiaArea = country.area()                    #This API returns area (km²) for a specified country
+indiaBorder = country.borders()               #This API bordering countries (ISO3) for a specified country
+print("List of all provinces of %s :\n"%(countryName))
 print(indiaProvinces)
 print("\n")
-print("List of alternate names of India : \n")
+print("List of alternate names of %s : \n"%(countryName))
 print(indiaAlternateNames)
+print("\n")
+print("Total Area of %s : \n"%(countryName))
+print(indiaArea)
+print("\n")
+print("Names of countries having borders with %s  : \n"%(countryName))
+print(indiaBorder)
+print("\n")
 input()
